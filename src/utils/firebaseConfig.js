de7ad1d9b34c43e914df9de1c7a1787a5682447b
Kallut <160795136+Kallutt@users.noreply.github.com>
2025-11-23 16:25:48 -0300

    Update firebaseConfig.js
    Mudança na chave API

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -7,7 +7,7 @@
 // CONFIGURAÇÃO DO FIREBASE
 // =================================================================
 export const FIREBASE_PROJECT_ID = 'appigreja-23c34';
-export const FIREBASE_WEB_API_KEY = 'AIzaSyAqsqHx3bK0kdkKfv185ct-5HAeVV-VCpY';
+export const FIREBASE_WEB_API_KEY = 'AIzaSyDkX2PqLwNcJm5rTv18d9-2GBfUU-KAjH'; //API usada para exemplo, não tem utilidade
 
 // Endpoint da API do Firestore para operações de documentos (CRUD)
 export const FIRESTORE_BASE_URL = `https://firestore.googleapis.com/v1/projects/${FIREBASE_PROJECT_ID}/databases/(default)/documents`;
@@ -15,4 +15,5 @@ export const FIRESTORE_BASE_URL = `https://firestore.googleapis.com/v1/projects/
 // Endpoints da API de Autenticação do Firebase Identity Toolkit
 export const AUTH_SIGN_IN_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_WEB_API_KEY}`;
 export const AUTH_SIGN_UP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_WEB_API_KEY}`;
+
 export const AUTH_RESET_PASSWORD_URL = `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${FIREBASE_WEB_API_KEY}`;  
